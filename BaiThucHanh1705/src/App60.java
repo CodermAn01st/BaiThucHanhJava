ea260a8585fc990df8afbaee60a1a8e1f4498b85
Nguyen Nam <2002namnguyenvan@gmail.com>
2022-05-17 22:40:17 +0700

    BaiThucHanh1705

---
+++ BaiThucHanh1705/src/App60.java
@@ -0,0 +1,16 @@
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class App60 {
+  public static void main(String[] args) {
+    Set<String> linkHasSet = new LinkedHashSet<>();
+    linkHasSet.add("Java");
+    linkHasSet.add("C++");
+    linkHasSet.add("Java");
+    linkHasSet.add("PHP");
+
+    for(String str : linkHasSet) {
+      System.out.println(str);
+    }
+  }
+}
